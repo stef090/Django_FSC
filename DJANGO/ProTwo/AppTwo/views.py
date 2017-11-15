fba430b29973118f335747cd6e4aa4bab59e01e0
stef090 <stjepan.lovasic@gmail.com>
2017-11-15 14:42:04 +0100

    User registration form, ModelForm exercise

--- DJANGO/ProTwo/AppTwo/views.py
+++ DJANGO/ProTwo/AppTwo/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.urls import reverse
 from AppTwo.models import User
+from . import forms
 
 # Create your views here.
 def index(request):
@@ -11,6 +13,17 @@ def help(request):
     return render(request,"AppTwo/help.html",context=context)
 
 def users(request):
-    users = User.objects.all()
-    context = {'users': users}
-    return render(request,"AppTwo/users.html",context=context)
+    # From Level Two
+    # users = User.objects.all()
+    # context = {'users': users}
+    form=forms.UserForm()
+    if request.method == "POST":
+        form = forms.UserForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print("FORM INVALID!")
+
+
+    return render(request,"AppTwo/users.html",{'form':form})
