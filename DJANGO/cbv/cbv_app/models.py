7f3709fc9f2eb0b9688c37d49eec3a483d081c9f
stef090 <stjepan.lovasic@gmail.colm>
2017-12-05 16:22:24 +0100

    Added CRUD views and templates

--- DJANGO/cbv/cbv_app/models.py
+++ DJANGO/cbv/cbv_app/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.core.urlresolvers import reverse
 
 
 class School(models.Model):
@@ -11,6 +12,9 @@ class School(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse("cbv_app:detail", kwargs={'pk': self.pk})
 
 
 class Student(models.Model):
--- DJANGO/cbv/cbv_app/views.py
+++ DJANGO/cbv/cbv_app/views.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-from django.views.generic import View, TemplateView, ListView, DetailView
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import (View,
+                                  TemplateView,
+                                  ListView, DetailView,
+                                  CreateView,UpdateView,DeleteView)
 from . import models
 
 class IndexView(TemplateView):
@@ -22,5 +25,19 @@ class SchoolDetailView(DetailView):
     context_object_name = 'school_detail'
     model = models.School
     template_name = 'cbv_app/school_detail.html'
+
+
+class SchoolCreateView(CreateView):
+    fields = ('name', 'principal', 'location')
+    model = models.School
+
+
+class SchoolUpdateView(UpdateView):
+    fields = ('name', 'principal')
+    model = models.School
+
+class SchoolDeleteView(DeleteView):
+    model = models.School
+    success_url = reverse_lazy("cbv_app:list")
 
 
