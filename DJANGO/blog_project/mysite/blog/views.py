8223eef41177f87ff5a8b0106c8b71b54132f601
stef090 <stjepan.lovasic@gmail.colm>
2017-12-07 10:53:14 +0100

    urls and views contd

--- DJANGO/blog_project/mysite/blog/views.py
+++ DJANGO/blog_project/mysite/blog/views.py
@@ -1,5 +1,56 @@
 from django.shortcuts import render
-from django.views.generic import (TemplateView)
+from .models import Post, Comment
+from django.utils import timezone
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from blog.forms import PostForm, CommentForm
+from django.views.generic import (TemplateView,
+                                  ListView, DetailView,
+                                  CreateView,UpdateView, DeleteView)
+
 
 class AboutView(TemplateView):
     template_name = 'about.html'
+
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now().order_by('-published_date'))
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    # Since we inherited from LoginRequiredMixin, we need to specify a login url
+
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
+
+
+class DraftListView(LoginRequiredMixin, ListView):
+
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_list.html'
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
