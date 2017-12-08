4993c6a4573ee429c1b5041c97332485b05b4790
stef090 <stjepan.lovasic@gmail.colm>
2017-12-08 12:39:35 +0100

    Error corrected, added css for loader and filled out base template

--- DJANGO/blog_project/mysite/blog/views.py
+++ DJANGO/blog_project/mysite/blog/views.py
@@ -19,7 +19,7 @@ class PostListView(ListView):
     model = Post
 
     def get_queryset(self):
-        return Post.objects.filter(published_date__lte=timezone.now().order_by('-published_date'))
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 
 
 class PostDetailView(DetailView):
